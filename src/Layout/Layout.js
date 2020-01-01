3e3704bdbd0b947a68c05f5699d8f05fc1acd760
Bartłomiej Sienko <bartlomiejsienko@MacBook-Air-Bartomiej.local>
2020-01-01 15:52:50 +0100

    separate globlstyle and theme to layout component

---
+++ src/Layout/Layout.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Qwigley&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap');
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+    // colors
+    main_red: "#FF0000",
+    main_white: "#FFFFFF",
+
+    // fonts
+    header1_font: "'Qwigley', cursive",
+    header2_font: "'Open Sans', sans-serif",
+
+    // transitions
+    main_transition: "all 0.7s cubic-bezier(.17,.67,0,.99)",
+}
+
+const Layout = ({ children }) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <React.Fragment>
+                <GlobalStyle />
+                {children}
+            </React.Fragment>
+        </ThemeProvider>
+
+    )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,9 @@
 import React from "react"
-import Navigation from '../components/Navigation'
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import styled from 'styled-components'
 
+import Layout from '../Layout/Layout'
+import Navigation from '../components/Navigation'
 import Hero from '../components/Hero'
-
-
-const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css?family=Qwigley&display=swap');
-    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap');
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-    // colors
-    main_red: "#FF0000",
-    main_white: "#FFFFFF",
-
-    // fonts
-    header1_font: "'Qwigley', cursive",
-    header2_font: "'Open Sans', sans-serif",
-
-    // transitions
-    main_transition: "all 0.7s cubic-bezier(.17,.67,0,.99)",
-}
 
 const AppWrapper = styled.div`
     transition: .2s ease;
@@ -39,14 +15,11 @@ const AppWrapper = styled.div`
 
 export default () => {
     return (
-        <ThemeProvider theme={theme}>
-            <React.Fragment>
-                <GlobalStyle />
-                <Navigation />
-                <AppWrapper className="main__app__wrapper">
-                    <Hero />
-                </AppWrapper>
-            </React.Fragment>
-        </ThemeProvider>
+        <Layout>
+            <Navigation />
+            <AppWrapper className="main__app__wrapper">
+                <Hero />
+            </AppWrapper>
+        </Layout>
     )
 }
