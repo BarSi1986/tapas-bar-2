4d15dff56ec1753302c9d09d2f8c1736d0600573
Bartłomiej Sienko <bartlomiejsienko@MacBook-Air-Bartomiej.local>
2020-01-01 12:57:21 +0100

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,16 @@ import React from "react"
 import Navigation from '../components/Navigation'
 import { createGlobalStyle } from 'styled-components'
 
+
 const GlobalStyle = createGlobalStyle`
-*{
+  *{
     @import url('https://fonts.googleapis.com/css?family=Qwigley&display=swap');
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap');
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
-`;
+  }
+`
 
 export default () => {
     return (
