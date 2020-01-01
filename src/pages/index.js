8527380fb228012dfde0bd0672375fd4203bb1f6
Bartłomiej Sienko <bartlomiejsienko@MacBook-Air-Bartomiej.local>
2020-01-01 13:15:37 +0100

    3

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Qwigley&display=swap');
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap');
-  body{
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
